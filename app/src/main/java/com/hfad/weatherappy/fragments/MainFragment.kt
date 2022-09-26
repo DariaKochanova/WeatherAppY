d9dc5aa8bc6471bb22a853c08280543a65cd4766
dariakochanova <kochanova.d.a@gmail.com>
2022-09-26 11:21:01 +0300

    delete ActionBar in buildGradle:    ViewBinding    implementetion volley, picasso, location, fragment in manifest    internet    location    use http (usesCleartextTraffic) create empty fragment MainFragment create empty class viewModel MainViewModel

---
+++ app/src/main/java/com/hfad/weatherappy/fragments/MainFragment.kt
@@ -0,0 +1,27 @@
+package com.hfad.weatherappy.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.hfad.weatherappy.R
+import com.hfad.weatherappy.databinding.FragmentMainBinding
+
+class MainFragment : Fragment() {
+    private lateinit var binding: FragmentMainBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    companion object {
+
+        fun newInstance() = MainFragment()
+
+    }
+}
